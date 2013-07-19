f35c3604b325a62de53507784266bd4c020409fb
Benjamin Guillet <benjamin.guillet@gmail.com>
2013-07-19 09:47:03 -0700

    Add column nickname.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130719163655) do
+ActiveRecord::Schema.define(version: 20130719164458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130719163655) do
     t.string  "first_name"
     t.string  "last_name"
     t.integer "score"
+    t.string  "nickname",   null: false
   end
 
 end
